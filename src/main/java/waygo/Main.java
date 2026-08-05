b5abf351e23d45199394ae7931a206bb03d5b1d9
Mikayıl Quliyev <mikayilquliyev16@gmail.com>
2026-08-05 10:50:10 +0400

    feat: add Explicit @EntityScan and @EnableJpaRepositories for JPA entities

--- src/main/java/waygo/Main.java
+++ src/main/java/waygo/Main.java
@@ -2,8 +2,12 @@ package waygo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "waygo")
+@SpringBootApplication(scanBasePackages = {"waygo", "org.example"})
+@EntityScan(basePackages = {"waygo", "org.example"})
+@EnableJpaRepositories(basePackages = {"waygo", "org.example"})
 public class Main {
 
     public static void main(String[] args) {
